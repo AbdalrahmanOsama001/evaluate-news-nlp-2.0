882034e86feeebd6273d6dfc8a1e7b1517f4c7ba
AbdalrahmanOsama001 <abdalrahman.osama01@gmail.com>
2024-12-03 00:18:09 +0200

    final version

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/client/js/app.js', // Your main JavaScript file
+    entry: './src/client/js/app.js',
     output: {
         filename: 'bundle.js', // Output JavaScript bundle
         path: path.resolve(__dirname, 'dist'), // Output directory
